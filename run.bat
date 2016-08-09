a8734c1636cf8ec4a39b30515bc7f1abf01deff4
marom <maromf9@gmail.com>
2016-08-09 19:45:06 +0300

    maromf change theam

--- run.bat
+++ run.bat
@@ -1,14 +1,22 @@
-@echo We are about to setup the enviornment so you could check this exercise.
-@echo Please verify that all executables from mongoDB, NodeJS and Google Chrome are in direcories defined in your PATH enviornment variable.
-@echo Also make sure that the path to the directory running this bat file doesn't contain hebrew letters nor spaces.
-@echo Please notice that the server is running on port 8080
-@echo --------------------------------------------------------------------------------------------------------------------------------------
+@echo *****************************************************************************************************************************************
+@echo Wellcom to PockaDailyAds.
+@echo Requirements:
+@echo      1. mongoDB
+@echo      2. NodeJS
+@echo      3. Google Chrome
 @echo
-@echo Starting mongod process 
+@echo - All executables are in directories defined in %PATH%.
+@echo - Path to Running Dir must not include hebrew letters.
+@echo
+@echo running on port 8080
+@echo *****************************************************************************************************************************************
+@echo
+@echo Initializing mongod process
 @echo
 start cmd /k mongod --dbpath server/data --logpath server/log/db.elg
 ping -n 6 127.0.0.1 >nul
 
+@echo ... 25%
 @echo
 @echo Injecting data.json file to mongoDB using mongoimport process
 @echo
@@ -16,12 +24,14 @@ mongoimport --db ads --collection messages --file server/messages.json --jsonArr
 mongoimport --db ads --collection admins --file server/admins.json
 ping -n 6 127.0.0.1 >nul
 
+@echo ... 50%
 @echo
-@echo Starting nodeJS server/main.js
+@echo Initializing nodeJS server/main.js
 @echo 
 start cmd /K node server/main.js
 ping -n 6 127.0.0.1 >nul
 
+@echo ... 75%
 @echo
 @echo Running chrome with screen parameter equals to 1,2,3
 @echo 
@@ -29,3 +39,6 @@ start chrome http://localhost:8080/screen=1
 start chrome http://localhost:8080/screen=2
 start chrome http://localhost:8080/screen=3
 start chrome http://localhost:8080/admin
+
+@echo ... 100%
+@echo
